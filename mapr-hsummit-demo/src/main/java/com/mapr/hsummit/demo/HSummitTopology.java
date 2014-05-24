59e40270faf22c9118ea5f07df5701b50f07cca7
Alex Rodrigues <arodrigues@mapr.com>
2014-05-24 14:10:10 +0100

    Attempt to create executable fat-jar. Wrote another way to put a reference to the resource R script

--- mapr-hsummit-demo/src/main/java/com/mapr/hsummit/demo/HSummitTopology.java
+++ mapr-hsummit-demo/src/main/java/com/mapr/hsummit/demo/HSummitTopology.java
@@ -9,11 +9,14 @@ import backtype.storm.topology.TopologyBuilder;
  * Created by syoon on 5/15/14.
  */
 public class HSummitTopology {
-    static String workingDir = System.getProperty("user.dir");
-    static String GAMELOGFILE = workingDir + "/src/main/resources/GL2012.TXT";
-    static String RSCRIPT = workingDir + "/src/main/resources/storm.R";
-    public static void main(String[] args) throws Exception {
+//    static String workingDir = System.getProperty("user.dir");
+//    static String GAMELOGFILE = workingDir + "/src/main/resources/GL2012.TXT";
+//    static String RSCRIPT = workingDir + "/src/main/resources/storm.R";
 
+    static String GAMELOGFILE = HSummitTopology.class.getResource("/GL2012.TXT").getFile();
+    static String RSCRIPT = HSummitTopology.class.getResource("/storm.R").getFile();
+
+    public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("spout", new MLBGameLogSpout(), 1);
         builder.setBolt("count", new RBolt(), 1).shuffleGrouping("spout");
